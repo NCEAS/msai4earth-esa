53da94784453a6591cd91076d142688bf8665f28
carmengg <c_galazgarcia@ucsb.edu>
2022-06-03 21:18:36 +0000

    adding polygon and naip scene id to point sampels

--- iceplant_detection/data_sampling/extracting_points_from_polygons.py
+++ iceplant_detection/data_sampling/extracting_points_from_polygons.py
@@ -22,7 +22,6 @@ def get_item_from_id(itemid):
     URL = "https://planetarycomputer.microsoft.com/api/stac/v1"
     catalog = pystac_client.Client.open(URL)
 
-    # campus point naip scene
     search = catalog.search(
         collections=["naip"],
         ids = itemid)
@@ -45,7 +44,8 @@ def get_raster_from_item(item):
 def num_random_points(polys, naip, proportion=0.2):
     pixel_size = naip.res[0]*naip.res[1]
     
-    # calculating how many pixels are there in the polygon (approx), by dividing the area of poly by area of a single pixel
+    # calculating how many pixels are there in the polygon (approx)
+    # by dividing the area of poly by area of a single pixel
     return polys.geometry.apply(lambda p: int((p.area/pixel_size)*proportion))
 
 # ---------------------------------------------
@@ -63,42 +63,43 @@ def random_pts_poly(number, polygon):
     return points  
 
 # ---------------------------------------------
-# polys needs to have at least geometry and iceplant columns 
+# polys needs to have at least geometry, iceplant and id columns 
 
 def sample_naip(polys, num_random_pts, naip, item):
     samples = []
     for i in range(0,polys.shape[0]):   # for each polygon in set
-    #for i in range(0,3):
         poly = polys['geometry'][i]
         N = num_random_pts[i]
 
         points = random_pts_poly(N,poly)  # select random points within poly
         sample = pd.DataFrame({
             'geometry': pd.Series(points), 
-            'iceplant':pd.Series(np.full(N,polys['iceplant'][i]))  # add iceplant Y/N identification for all pts
+            'iceplant':pd.Series(np.full(N,polys['iceplant'][i])),  # add iceplant Y/N identification for all pts
+            'polygon_id':pd.Series(np.full(N,polys['id'][i]))
                      })
 
-        sample['xy']=sample.geometry.apply(lambda p :(p.x, p.y))   # separate coords for reasterio.io.DatasetReader sample function
+        sample['xy'] = sample.geometry.apply(lambda p :(p.x, p.y))  # separate coords (needed for reasterio.io.DatasetReader sample function)
         data_generator = naip.sample(sample.xy)   # extract band values from naip image 
         data = []
-        for i in data_generator:
+        for i in data_generator: 
             data.append(i)
         data = np.vstack(data)
         data = pd.DataFrame(data, columns=['r','g','b','nir']) # create pd DataFrame from np.array
-
+        
         sample = pd.concat([sample,data],axis=1)  # add band data to points
         samples.append(sample)   # add new samples
 
     df = pd.concat(samples) # create dataframe from samples list
     
-    df['x']= df.geometry.apply(lambda p : p.x)   # some df cleaning
+    df['x']= df.geometry.apply(lambda p : p.x)   # coordinate cleaning
     df['y']= df.geometry.apply(lambda p : p.y)
     df.drop('xy',axis=1,inplace=True)
-    df = df[['geometry','x','y','iceplant','r','g','b','nir']]
     
     df['year'] = item.datetime.year   # add date to samples
     df['month'] = item.datetime.month
     df['day'] = item.datetime.day 
+    df['naip_id'] = item.id           # add naip item id to samples
+    df = df[['geometry','x','y','iceplant','r','g','b','nir','year','month','day','naip_id','polygon_id']]
     return df
 
 # ---------------------------------------------
