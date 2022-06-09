ab734160678980cf680da91a13192ab2ba9daa42
carmengg <c_galazgarcia@ucsb.edu>
2022-06-09 23:36:03 +0000

    trying to recover aff transf

--- iceplant_detection/iceplant_detection_functions.py
+++ iceplant_detection/iceplant_detection_functions.py
@@ -5,123 +5,10 @@ import matplotlib.pyplot as plt
 import rasterio
 import geopandas as gpd
 
-from sklearn.metrics import RocCurveDisplay
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import matthews_corrcoef
-from sklearn.metrics import fbeta_score
-
-from sklearn.model_selection import train_test_split
-
 import pystac_client 
 import planetary_computer as pc
 
-
-# **********************************************************************************************************
-# **********************************************************************************************************
-
-
-def test_train_from_df(df,test_size=0.3):
-    # Labels are the values we want to predict
-    labels = np.array(df['iceplant'])
-    #Convert to numpy array
-    features = np.array(df.drop('iceplant', axis = 1))
-    return train_test_split(features, labels, 
-                            test_size = test_size, 
-                            random_state = 42)
-
-# --- print shapes of train/test features/labels
-def  train_test_shapes(train_features, train_labels, test_features, test_labels):
-    print('Training Features Shape:', train_features.shape) 
-    print('Training Labels Shape:', train_labels.shape) 
-    print('Testing Features Shape:', test_features.shape) 
-    print('Testing Labels Shape:', test_labels.shape)
-    print()
-    return
-
-# --- print proportions of ice plant (1) vs no iceplant (0) in an array with only 0 and 1
-def iceplant_proportions(labels):
-    unique, counts = np.unique(labels, return_counts=True)
-    print('no-iceplant:iceplant ratio    ',round(counts[0]/counts[1],1),':1')
-    n = labels.shape[0]
-    perc = [round(counts[0]/n*100,2), round(counts[1]/n*100,2)]
-    df = pd.DataFrame({'iceplant':unique,
-             'counts':counts,
-             'percentage':perc}).set_index('iceplant')
-    print(df)
-    print()
-
-# --- print proportions of ice plant vs no ice plant in train/test sets
-def test_train_proportions(train_labels, test_labels):
-    print('TRAIN SET COUNTS:')
-    iceplant_proportions(train_labels)
-
-    print('TEST SET COUNTS:')
-    iceplant_proportions(test_labels)
-
-    return
-
-
-# **********************************************************************************************************
-# **********************************************************************************************************
-
-# https://stackoverflow.com/questions/61466961/what-do-the-normalize-parameters-mean-in-sklearns-confusion-matrix
-# *** change to y_true / y_pred
-
-def print_accuracy_info(y_true,y_pred):
-    N = y_true.shape[0]
-    
-    confmtx = confusion_matrix(y_true,y_pred)
-    
-    print('true negatives:', confmtx[0,0], 
-          '    false positives:', confmtx[0,1])
-    print('false negatives:', confmtx[1,0], 
-          '    true positives:', confmtx[1,1])
-    print()
-    unique, counts = np.unique(y_true,return_counts=True)
-    
-    sens =  confmtx[1,1]/counts[1]
-    spec =  confmtx[0,0]/counts[0]
-    print('sensitivity (TP/P):', np.round(sens*100,2), '%')  
-    print('specificity (TN/N):', np.round(spec*100,2), '%')  
-    print('G-mean: ', round(np.sqrt(sens*spec),2))
-    print()
-    
-    prec = confmtx[1,1]/(confmtx[1,1]+confmtx[0,1])
-    print('precision (TP/(TP+FP)):', np.round(prec*100,2),'%' )
-    print()
-    
-    print('MCC: ', matthews_corrcoef(y_true,y_pred))
-    print()
-    
-    print('F1-measure: ',  round(fbeta_score(y_true, y_pred, beta=1.0),5))
-    print('F0.5-measure (min false positives): ',  round(fbeta_score(y_true, y_pred, beta=0.5),5))
-    print('F2-measure (min false negatives)  : ',  round(fbeta_score(y_true, y_pred, beta=2.0),5))
-    print()
-        
-    print('accuracy:', np.round( (confmtx[1,1] + confmtx[0,0])/y_true.shape[0]*100,2),'%') # (TP + TN)/(P + N)
-    return
-
-# ---------------------------------
-def plot_roc(rfc, test_features, test_labels):
-    ax = plt.gca()
-    rf_disp = RocCurveDisplay.from_estimator(rfc, test_features, test_labels, ax=ax)
-    return
-
-# ---------------------------------
-def print_abs_errors(predictions_class, test_labels):# Calculate the absolute errors
-    errors_class = abs(predictions_class - test_labels)
-    # Print out the mean absolute error (mae)
-    print('Mean Absolute Error:', round(np.mean(errors_class), 2))
-    return
-
-# ---------------------------------
-def print_threshold_metrics(test_labels, predictions):
-    print()
-    print_accuracy_info(test_labels,predictions)
-    #plot_roc(rfc, test_features, test_labels)
-    print()
-    return
-
+import calendar
 
 # **********************************************************************************************************
 # **********************************************************************************************************
@@ -147,7 +34,7 @@ def get_raster_from_item(item):
     ds = rasterio.open(href)
     return ds
 
-# *********************************************************************
+# ---------------------------------
 
 def href_and_window(itemid, reduce_box):
     item = get_item_from_id(itemid)
@@ -159,6 +46,24 @@ def href_and_window(itemid, reduce_box):
     reduce = reduce.to_crs(ds.crs)
     win = ds.window(*reduce.total_bounds)
     return href, win
+
+# **********************************************************************************************************
+# **********************************************************************************************************
+
+import rioxarray as rioxr
+
+def small_raster(href, reduce_box):
+    item = get_item_from_id(itemid)
+    # sign and open item
+    href = pc.sign(item.assets["image"].href)
+    
+    rioxr.open_rasterio(href)
+    rgb_small = rgb.rio.clip_box(*reduce.total_bounds)
+    return rgb_small
+
+# **********************************************************************************************************
+# **********************************************************************************************************
+
 
 # ---------------------------------
 
@@ -172,7 +77,7 @@ def rgb_window_in_scene(itemid, reduce_box):
     href, win = href_and_window(itemid, reduce_box)   
     return rasterio.open(href).read([1,2,3], window=win)
 
-# ---------------------------------
+# **********************************************************************************************************
 
 def plot_window_in_scene(itemid, reduce_box, figsize=15):
     
@@ -183,23 +88,14 @@ def plot_window_in_scene(itemid, reduce_box, figsize=15):
 
 
 # **********************************************************************************************************
-# **********************************************************************************************************
 
-def predict_over_subset(itemid, reduce_box, rfc):
-    image = open_window_in_scene(itemid, reduce_box)
-    # reshape image into a np.array where each row is a pixel and the columns are the bands
-    pixels = image.reshape([4,-1]).T
-    predictions_class = rfc.predict(pixels)
-    # turn back into original raster dimensions
-    return predictions_class.reshape([image.shape[1],-1])
-
-# **********************************************************************************************************
-# **********************************************************************************************************
 
 # image is a (4,m,n) np array in which bands are r,g,b,nir
 def ndvi(image):
     x = image.astype('int16')
     return (x[3,...] - x[0,...])/(x[3,...] + x[0,...])
+
+# ---------------------------------
 
 def ndvi_thresh(image, thresh=0.05):
     x = ndvi(image)
@@ -208,26 +104,16 @@ def ndvi_thresh(image, thresh=0.05):
     x[~low_ndvi] = 1
     return x
 
+# ---------------------------------
+
 def select_ndvi_image(itemid, reduce_box, thresh=0.05):
     image = open_window_in_scene(itemid, reduce_box)
     return ndvi_thresh(image,thresh)
 
-# **********************************************************************************************************
-# **********************************************************************************************************
 
-
+# **********************************************************************************************************
 
 # image is a (4,m,n) np array in which bands are r,g,b,nir
-
-# def select_ndvi_df(image, thresh=0.05):
-#     # reshape image into a np.array where each row is a pixel and the columns are the bands
-#     pixels = image.reshape([4,-1]).T
-#     df = pd.DataFrame(pixels, columns=['r','g','b','nir'])
-#     df['ndvi'] = (df.nir.astype('int16') - df.r.astype('int16'))/(df.nir.astype('int16') + df.r.astype('int16'))
-#     vegetation = df[df.ndvi>thresh]
-#     vegetation.drop(labels=['ndvi'], axis=1, inplace=True)
-#     return vegetation
-# ---------------------------------
 
 def select_ndvi_df(image, thresh=0.05):
     pixels = image.reshape([4,-1]).T
@@ -242,43 +128,6 @@ def select_ndvi_df(image, thresh=0.05):
 
 # ---------------------------------
 
-# OLD FUNCTION: SLOOOOOOW!
-# def predictions_backto_image(image, df):
-#     reconstruct = np.zeros((image.shape[1],image.shape[2]))
-#     for n in df.index:
-#         if df.prediction[n]==1:
-#             i = int((n)/reconstruct.shape[1])
-#             j = (n) % reconstruct.shape[1]
-#             reconstruct[i][j] = 1
-#     return reconstruct
-
-
-# def predictions_backto_image(nrows, ncols, df):
-    
-#     index = df[df.prediction == 1].index.to_numpy()
-#     i = index / ncols
-#     i = i.astype(int)
-#     j = index % ncols
-#     reconstruct = np.zeros((nrows,ncols))
-#     reconstruct[i,j] = 1
-#     return reconstruct
-
-# # ---------------------------------
-# def mask_ndvi_and_predict(itemid, reduce_box, rfc, thresh=0.05):
-#     image = open_window_in_scene(itemid, reduce_box)
-#     veg = select_ndvi_df(image, thresh)
-#     index = veg.index
-#     features = np.array(veg)
-#     predictions_class = rfc.predict(features)
-#     c = {'prediction':predictions_class}
-#     predictions_df = pd.DataFrame(c, index = index)
-    
-#     nrows = image.shape[1]
-#     ncols = image.shape[2]
-    
-#     return predictions_backto_image(nrows, ncols, predictions_df)
-
-
 def indices_backto_image(nrows, ncols, index):
     # transform indices to coordinates
     i = index / ncols
@@ -290,7 +139,19 @@ def indices_backto_image(nrows, ncols, index):
     reconstruct[i,j] = 1
     return reconstruct
 
+# **********************************************************************************************************
+
+def predict_over_subset(itemid, reduce_box, rfc):
+    image = open_window_in_scene(itemid, reduce_box)
+    # reshape image into a np.array where each row is a pixel and the columns are the bands
+    pixels = image.reshape([4,-1]).T
+    predictions_class = rfc.predict(pixels)
+    # turn back into original raster dimensions
+    return predictions_class.reshape([image.shape[1],-1])
+
 # ---------------------------------
+
+# rfc must only take r, g, b, nir as featuers (IN THAT ORDER)
 def mask_ndvi_and_predict(itemid, reduce_box, rfc, thresh=0.05):
     image = open_window_in_scene(itemid, reduce_box)
     veg = select_ndvi_df(image, thresh)
@@ -311,27 +172,28 @@ def mask_ndvi_and_predict(itemid, reduce_box, rfc, thresh=0.05):
 
 
 # # **********************************************************************************************************
-# **********************************************************************************************************
+# def day_in_year(day,month,year):
+#     days_in_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+#     n = 0
+#     for i in range(0,month-1):
+#         n = n+days_in_month[i]
+#     n = n+day
+#     if calendar.isleap(year) and month>2:
+#         n = n+1
+#     return n
 
+# # ---------------------------------
 
-# def select_ndvi_image(itemid, reduce_box, thresh=0.05):
-#     subset = open_window_in_scene(itemid, reduce_box)
-#     df = select_ndvi_df(subset, thresh)
-#     reconstruct = np.zeros((subset.shape[1],subset.shape[2]))
-#     for n in df.index:
-#         i = int((n)/reconstruct.shape[1])
-#         j = (n) % reconstruct.shape[1]
-#         reconstruct[i][j] = 1
-#     return reconstruct
+# def add_date_features(item, df):
 
+# #    veg = ipf.select_ndvi_df(image)
+    
+# #    veg['ndvi']=(veg.nir.astype('int16') - veg.r.astype('int16'))/(veg.nir.astype('int16') + veg.r.astype('int16'))
 
+#     df['year'] = item.datetime.year
+#     df['month'] = item.datetime.month
+#     df['day_in_year'] = day_in_year(item.datetime.day, item.datetime.month, item.datetime.year)
 
-# ---------------------------------
+#     veg = veg[['r','g','b','nir','ndvi','year','month','day_in_year']] # order features
+#     return df
 
-# def ndvi_df_backto_image(image, df):
-#     reconstruct = np.zeros((image.shape[1],image.shape[2]))
-#     for n in df.index:
-#         i = int((n)/reconstruct.shape[1])
-#         j = (n) % reconstruct.shape[1]
-#         reconstruct[i][j] = 1
-#     return reconstruct
