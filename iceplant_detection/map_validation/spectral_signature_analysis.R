5cf92d00e978e12700c9dd8c58750d7479067de5
Julien Brun <brun.julien@gmail.com>
2022-11-27 20:41:13 -0800

    producing the spectral signature plot

--- iceplant_detection/map_validation/spectral_signature_analysis.R
+++ iceplant_detection/map_validation/spectral_signature_analysis.R
@@ -1,9 +1,11 @@
-
+# Inspecting spectral signature of different classification confusions
 
 librarian::shelf(tidyverse, janitor, naniar, sf)
 
+data_dir <- "/Users/brun/Data/msai4earth/naip_iceplant_2020/validation_results_spectral_2020"
+
 # import the data
-data_assessmt <- st_read("naip_iceplant_2020/validation_results_spectral_2020/validation_results_spectral_2020.shp") %>%
+data_assessmt <- st_read(file.path(data_dir, "validation_results_spectral_2020.shp")) %>%
   replace_with_na_if(.predicate = is.numeric,
                      condition = ~.x == -9999)
 
@@ -31,46 +33,44 @@ data_assessmt_flag <- data_assessmt %>%
   ))
 
 
+
+#### COMPUTE MEAN FOR ICEPLANT SIGNATURES ####
+
 # Subset the "pure" spectral signatures
 truepos_signatures <- data_assessmt_flag %>% 
-  filter(str_detect(category_flag, "true_pos")) 
+  filter(str_detect(category_flag, "iceplant")) 
 
 # Compute the average "pure" spectral signatures
-# truepos_signatures_mean <- truepos_signatures %>%
-#   group_by(category_flag, .drop = FALSE) %>%
-#   summarise_if(is.numeric, mean, na.rm = TRUE) %>% 
-#   as.data.frame() %>%
-#   select(-geometry)
-
-truepos_signatures_mean_sd <- truepos_signatures %>%
-  group_by(category_flag, .drop = FALSE) %>%
-  summarise(across(where(is.numeric), 
-                   list(mean = ~mean(.x, na.rm = TRUE), sd = ~sd(.x, na.rm = TRUE)),
-                   .names = "{.col}.{.fn}")) %>% 
+truepos_signatures_mean <- truepos_signatures %>%
+  select(-c(year, month, day_in_yea, pl_which_r, low_confid, map_class, ref_class)) %>% # remove unwanted columns
+  group_by(category_flag, .drop = FALSE) %>%  # Create groups
+  summarise(across(where(is.numeric),   # compute the mean
+                   list(mean = mean),
+                   na.rm = TRUE,
+                   .names = "{.col}.{.fn}"),
+            count = n()) %>%  # add the count of the groups
   as.data.frame() %>%
-  select(-geometry)
+  select(-geometry)  # remove geometry
 
 
-ggplot(truepos_signatures) + 
-  geom_point(aes(x=category_flag, y = r), color = "red") +
-  geom_point(aes(x=category_flag, y = g), color = "green") +
-  geom_point(aes(x=category_flag, y = b), color = "blue") +
-  geom_point(aes(x=category_flag, y = nir), color = "dark red") +
-  geom_point(aes(x=category_flag, y = lidar), color = "grey") +
-  coord_flip()
+# make it a long format so ggplot is happy
+truepos_signatures_mean_long <- truepos_signatures_mean %>%
+  pivot_longer(cols = ends_with("mean"),
+               names_to = "channel",
+               values_to = "mean") %>%
+  filter(!str_detect(channel, "lidar")) %>%
+  mutate(channel = factor(channel, 
+                                levels = c("b.mean", "g.mean", "r.mean", "nir.mean", 
+                                           "lidar.mean", "min_lidar.mean", "avg_lidar.mean", "max_lidar.mean")))
 
-# Compute the "pure" spectral signatures
-iceplant_false_signatures <- data_assessmt_flag %>% 
-  filter(str_detect(category_flag, "_pos")) %>%
-  group_by(category_flag, .drop = FALSE) %>%
-  summarise_if(is.numeric, mean, na.rm = TRUE) %>% 
-  as.data.frame() %>%
-  select(-geometry)
 
-ggplot(truepos_signatures) + 
-  geom_point(aes(x= category_flag, y = r), color = "red") +
-  geom_point(aes(x= category_flag, y = g), color = "green") +
-  geom_point(aes(x= category_flag, y = b), color = "blue") +
-  geom_point(aes(x= category_flag, y = nir), color = "dark red") +
-  geom_point(aes(x= category_flag, y = lidar), color = "grey") +
-  coord_flip()
+
+
+# plot the spectral signatures
+ggplot(truepos_signatures_mean_long, aes(x = channel, y = mean, color = category_flag, group = category_flag)) + 
+  geom_line() + 
+  geom_point() +
+  labs(color = "Category") +
+  ggtitle("Pure spectral signatures") +
+  theme_bw()
+
